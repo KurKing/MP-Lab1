6e9af327c4f2ad42944230ef6e0cde6f9aa88550
KurKing <lyosha.kurkin@gmail.com>
2022-02-13 22:17:12 +0200

    task 1 is done

--- task1.py
+++ task1.py
@@ -33,22 +33,28 @@ def main():
     if string[i] != ' ' and string[i] != '\n':
         word[lastSymbolIndex] = string[i]
         lastSymbolIndex += 1
+        if i == stringLength - 1:
+            goto .addWordToTheList
     else:
+        label .addWordToTheList
         wordLength = 0
         label .countWordLength
         if word[wordLength]!= 0:
             wordLength += 1
             goto .countWordLength
+
         if wordLength > 3:
             tempWordIndex = 0
             tempWord = ""
             label .createTempWord
-            tempWord += word[tempWordIndex]
+            asciiCharToAd = ord(word[tempWordIndex])
+            if(asciiCharToAd>64 and asciiCharToAd<91):
+                asciiCharToAd+=32
+            tempWord += chr(asciiCharToAd)
             tempWordIndex += 1
             if tempWordIndex < wordLength:
                 goto .createTempWord
             words.append(tempWord)
-
         word = [0]*20
         lastSymbolIndex = 0
 
@@ -56,8 +62,82 @@ def main():
     if i < stringLength:
         goto .dividingFileStringInWords
 
-    
+    # count amount of words
+    wordsAmount = 0
+    label .countWordsAmount
+    try:
+        words[wordsAmount]
+        wordsAmount += 1
+        goto .countWordsAmount
+    except IndexError:
+        pass
 
-    print(words)
+    # wordsFrequencyArray generating
+    wordsFrequencyArray = [0]*wordsAmount
+    wordIndex = 0
+    i = 0
+    label .wordsFrequencyArrayGenerating
+    if wordsFrequencyArray[i] == 0:
+        wordsFrequencyArray[i] = WordFrequency(words[wordIndex], 1)
+        wordIndex += 1
+        if wordIndex >= wordsAmount:
+            goto .endOfWordsFrequencyArrayGenerating
+        i = 0
+        goto .wordsFrequencyArrayGenerating
+    else:
+        if wordsFrequencyArray[i].word == words[wordIndex]:
+            wordsFrequencyArray[i].amount += 1
+            wordIndex += 1
+            if wordIndex >= wordsAmount:
+                goto .endOfWordsFrequencyArrayGenerating
+            i = 0
+            goto .wordsFrequencyArrayGenerating
+
+        i += 1
+        if i < wordsAmount:
+            goto .wordsFrequencyArrayGenerating
+
+    label .endOfWordsFrequencyArrayGenerating
+
+    # removing zeros from array
+    wordsFrequencyAmount = 0
+    label .countWordsFrequency
+    if wordsFrequencyArray[wordsFrequencyAmount] != 0:
+        wordsFrequencyAmount += 1
+        goto .countWordsFrequency
+
+    cleanWordsFrequencyArray = [0]*wordsFrequencyAmount
+    i = 0
+    label .generateCleanWordsFrequencyArray
+    cleanWordsFrequencyArray[i] = wordsFrequencyArray[i]
+    i += 1
+    if i < wordsFrequencyAmount:
+        goto .generateCleanWordsFrequencyArray
+
+    # sorting array
+    i = 0
+    label .bubbleSortFirstStage
+    j = 0
+    label .bubbleSortSecondStage
+    if cleanWordsFrequencyArray[j].amount < cleanWordsFrequencyArray[j+1].amount:
+        cleanWordsFrequencyArray[j], cleanWordsFrequencyArray[j+1] = cleanWordsFrequencyArray[j+1], cleanWordsFrequencyArray[j]
+    j += 1
+    if j < wordsFrequencyAmount-1-i:
+        goto .bubbleSortSecondStage
+    i += 1
+    if i < wordsFrequencyAmount-1:
+        goto .bubbleSortFirstStage
+
+    # generating final string
+    finalString = f"{cleanWordsFrequencyArray[0].word} - {cleanWordsFrequencyArray[0].amount}"
+    index = 1
+    label .generateFinalString
+    finalString += f"\n{cleanWordsFrequencyArray[index].word} - {cleanWordsFrequencyArray[index].amount}"
+    index += 1
+    if index < wordsFrequencyAmount:
+        goto .generateFinalString
+
+    # printing result
+    print(finalString)
 
 main()
